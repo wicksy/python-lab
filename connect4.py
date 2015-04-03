dc6780c99becb771eb77f37877af8429ec662e38
Martin Wicks <theboywicks@gmail.com>
2015-04-03 22:13:10 +0100

    Continue arg and options validation and add usage()

--- connect4.py
+++ connect4.py
@@ -12,6 +12,13 @@ board_rows = 6
 
 board = []
 
+def usage():
+    print("usage: connect4 -p port host")
+    print("Options and arguments :")
+    print("-p port\t: tcp port to listen on or connect to (1024-65535)")
+    print("host\t: IP address to connect with")
+    sys.exit(0)
+
 def print_board(board):
     for row in board:
         print (" ".join(row))
@@ -21,12 +28,23 @@ def initialise_board(board):
     for x in range(0,board_rows):
         board.append(["O"] * board_cols)
 
+def valid_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        host_bytes = ip.split('.')
+        valid = [int(b) for b in host_bytes]
+        valid = [b for b in valid if b >= 0 and b<=255]
+        return len(host_bytes) == 4 and len(valid) == 4
+    except:
+        return False
+
 def main():
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'p:')
+        opts, args = getopt.getopt(sys.argv[1:], 'p:h')
     except getopt.GetoptError as err:
         print str(err)
+        usage()
         sys.exit(100)
 
     for o, a in opts:
@@ -34,18 +52,36 @@ def main():
             try:
                 port = int(a)
             except:
-                print("Please specify port as integer > 1024 and < 65536")
+                print("Please specify port as integer > 1023 and < 65536")
+                usage()
                 sys.exit(105)
             else:
-                if port < 1025 or port > 65535:
+                if port < 1024 or port > 65535:
                     print("Port should be between 1024 and 65535")
+                    usage()
                     sys.exit(110)
+        elif o == "-h":
+            usage()
         else:
             assert False, "unhandled option"
 
-    print(opts)
-    print(args)
-    print(port)
+    if len(args) == 0:
+        print("Please specify IP address to play")
+        usage()
+        sys.exit(120)
+    elif len(args) > 1:
+        print("Extra arguments passed")
+        usage()
+        sys.exit(130)
+    else:
+        ip = ''.join(args)
+        if not valid_ip(ip):
+            print("Invalid IP address " + ip)
+            usage()
+            sys.exit(140)
+
+
+
 
     initialise_board(board)
     print_board(board)
